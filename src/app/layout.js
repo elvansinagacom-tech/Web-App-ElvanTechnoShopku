c9f495a69313406db3ba18efd1a9e9374a5681c1
elvansinagacom-tech <elvansinagacom@gmail.com>
2026-03-17 22:05:34 +0700

    Add files via upload

---
+++ src/app/layout.js
@@ -0,0 +1,22 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'ElvanTechnoShop',
+  description: 'Dibuat resmi oleh Elvan Parlin Agustario Sinaga',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
+export const viewport = {
+  themeColor: '#020617', // Ini adalah warna hex dari bg-slate-950 (Warna tema aplikasi Bos)
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false, // Mencegah user nge-zoom layar pakai 2 jari
+  viewportFit: 'cover', // INI MANTRA AJAIBNYA! (Biar aplikasi tembus ke belakang jam)
+};
