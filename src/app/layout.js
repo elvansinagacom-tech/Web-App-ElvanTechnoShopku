053923a4818e142f8ca36121d0bcfeee8291b8f0
elvansinagacom-tech <elvansinagacom@gmail.com>
2026-03-18 01:48:07 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
+    <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#020617" />
+      </head>
+      <body className={... (biarkan kode body bawaan Bos tetap di sini)}>
+        {children}
+      </body>
       <body>{children}</body>
     </html>
   )
