b69d9f8e64c7f05b94feb87881661c777e984241
elvansinagacom-tech <elvansinagacom@gmail.com>
2026-03-18 11:13:48 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,22 +3,22 @@ import './globals.css'
 export const metadata = {
   title: 'ElvanTechnoShop',
   description: 'Dibuat resmi oleh Elvan Parlin Agustario Sinaga',
-  manifest: '/manifest.json', 
+  manifest: '/manifest.json',
 }
 
 export const viewport = {
-  themeColor: '#020617', 
+  themeColor: '#020617',
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
-  userScalable: false, 
-  viewportFit: 'cover', 
+  userScalable: false,
+  viewportFit: 'cover',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body>
+      <body className="overscroll-y-none">
         {children}
       </body>
     </html>
